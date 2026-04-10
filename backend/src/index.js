4614823a829aae3867d4b8e270df011bf7c49126
prajwal-Ivoyant <prajwalp@ivoyant.com>
2026-04-10 20:17:43 +0530

    file alteration

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
-import connectDB from "./src/config/database.js";
-import productRoutes from "./src/routes/product.routes.js"
+import connectDB from "./config/database.js";
+import productRoutes from "./routes/product.routes.js"
 import cors from "cors"
 
 // const swaggerUi = require("swagger-ui-express");
